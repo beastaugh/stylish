6566a1740c2c7ba8d3bfe752da6cd481c108849a
Benedict Eastaugh <benedict@eastaugh.net>
2008-11-25 00:09:31 +0000

    Add test cases for Selector and Selectors.

---
+++ test/selector_test.rb
@@ -0,0 +1,13 @@
+require 'test/unit'
+require './lib/stylish'
+
+class SelectorTest < Test::Unit::TestCase
+
+  def test_to_string
+    s = Stylish::Selector.new(".test")
+    assert_equal(".test", s.to_s)
+    
+    s = Stylish::Selector.new(:div)
+    assert_equal("div", s.to_s)
+  end
+end
---
+++ test/selectors_test.rb
@@ -0,0 +1,21 @@
+require 'test/unit'
+require './lib/stylish'
+
+class SelectorsTest < Test::Unit::TestCase
+  
+  def setup
+    @ss = Stylish::Selectors.new
+    
+    (1..3).each do |i|
+      @ss << Stylish::Selector.new(".test_" + i.to_s)
+    end
+  end
+  
+  def test_join
+    assert_equal(".test_1, .test_2, .test_3", @ss.to_s)
+  end
+  
+  def test_to_string
+    assert_equal(".test_1, .test_2, .test_3", @ss.to_s)
+  end
+end
