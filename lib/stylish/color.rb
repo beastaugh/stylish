b2b7c13925407065bcbb8f184856998709b2f876
Benedict Eastaugh <benedict@eastaugh.net>
2009-01-22 23:42:55 +0000

    Opacity values for colour keywords should be nil.

--- lib/stylish/color.rb
+++ lib/stylish/color.rb
@@ -60,136 +60,136 @@ module Stylish #:nodoc:
       #
       # * {SVG colors}[http://www.w3.org/TR/SVG/types.html#ColorKeywords]
       # * {X11 colors}[http://msdn.microsoft.com/en-us/library/aa905747.aspx]
-      :aliceblue            => [240, 248, 255], # => #f0f8ff
-      :antiquewhite         => [250, 235, 215], # => #faebd7
-      :aquamarine           => [127, 255, 212], # => #7fffd4
-      :azure                => [240, 255, 255], # => #f0ffff
-      :beige                => [245, 245, 220], # => #f5f5dc
-      :bisque               => [255, 228, 196], # => #ffe4c4
-      :blanchedalmond       => [255, 235, 205], # => #ffebcd
-      :blueviolet           => [138, 43,  226], # => #8a2be2
-      :brown                => [165, 42,  42 ], # => #a52a2a
-      :burlywood            => [222, 184, 135], # => #deb887
-      :cadetblue            => [95,  158, 160], # => #5f9ea0
-      :chartreuse           => [127, 255, 0  ], # => #7fff00
-      :chocolate            => [210, 105, 30 ], # => #d2691e
-      :coral                => [255, 127, 80 ], # => #ff7f50
-      :cornflowerblue       => [100, 149, 237], # => #6495ed
-      :cornsilk             => [255, 248, 220], # => #fff8dc
-      :crimson              => [220, 20,  60 ], # => #dc143c
-      :cyan                 => [0,   255, 255], # => #00ffff
-      :darkblue             => [0,   0,   139], # => #00008b
-      :darkcyan             => [0,   139, 139], # => #008b8b
-      :darkgoldenrod        => [184, 134, 11 ], # => #b8860b
-      :darkgray             => [169, 169, 169], # => #a9a9a9
-      :darkgreen            => [0,   100, 0  ], # => #006400
-      :darkgrey             => [169, 169, 169], # => #a9a9a9
-      :darkkhaki            => [189, 183, 107], # => #bdb76b
-      :darkmagenta          => [139, 0,   139], # => #8b008b
-      :darkolivegreen       => [85,  107, 47 ], # => #556b2f
-      :darkorange           => [255, 140, 0  ], # => #ff8c00
-      :darkorchid           => [153, 50,  204], # => #9932cc
-      :darkred              => [139, 0,   0  ], # => #8b0000
-      :darksalmon           => [233, 150, 122], # => #e9967a
-      :darkseagreen         => [143, 188, 143], # => #8fbc8f
-      :darkslateblue        => [72,  61,  139], # => #483d8b
-      :darkslategray        => [47,  79,  79 ], # => #2f4f4f
-      :darkslategrey        => [47,  79,  79 ], # => #2f4f4f
-      :darkturquoise        => [0,   206, 209], # => #00ced1
-      :darkviolet           => [148, 0,   211], # => #9400d3
-      :deeppink             => [255, 20,  147], # => #ff1493
-      :deepskyblue          => [0,   191, 255], # => #00bfff
-      :dimgray              => [105, 105, 105], # => #696969
-      :dimgrey              => [105, 105, 105], # => #696969
-      :dodgerblue           => [30,  144, 255], # => #1e90ff
-      :firebrick            => [178, 34,  34 ], # => #b22222
-      :floralwhite          => [255, 250, 240], # => #fffaf0
-      :forestgreen          => [34,  139, 34 ], # => #228b22
-      :gainsboro            => [220, 220, 220], # => #dcdcdc
-      :ghostwhite           => [248, 248, 255], # => #f8f8ff
-      :gold                 => [255, 215, 0  ], # => #ffd700
-      :goldenrod            => [218, 165, 32 ], # => #daa520
-      :greenyellow          => [173, 255, 47 ], # => #adff2f
-      :grey                 => [128, 128, 128], # => #808080
-      :honeydew             => [240, 255, 240], # => #f0fff0
-      :hotpink              => [255, 105, 180], # => #ff69b4
-      :indianred            => [205, 92,  92 ], # => #cd5c5c
-      :indigo               => [75,  0,   130], # => #4b0082
-      :ivory                => [255, 255, 240], # => #fffff0
-      :khaki                => [240, 230, 140], # => #f0e68c
-      :lavender             => [230, 230, 250], # => #e6e6fa
-      :lavenderblush        => [255, 240, 245], # => #fff0f5
-      :lawngreen            => [124, 252, 0  ], # => #7cfc00
-      :lemonchiffon         => [255, 250, 205], # => #fffacd
-      :lightblue            => [173, 216, 230], # => #add8e6
-      :lightcoral           => [240, 128, 128], # => #f08080
-      :lightcyan            => [224, 255, 255], # => #e0ffff
-      :lightgoldenrodyellow => [250, 250, 210], # => #fafad2
-      :lightgray            => [211, 211, 211], # => #d3d3d3
-      :lightgreen           => [144, 238, 144], # => #90ee90
-      :lightgrey            => [211, 211, 211], # => #d3d3d3
-      :lightpink            => [255, 182, 193], # => #ffb6c1
-      :lightsalmon          => [255, 160, 122], # => #ffa07a
-      :lightseagreen        => [32,  178, 170], # => #20b2aa
-      :lightskyblue         => [135, 206, 250], # => #87cefa
-      :lightslategray       => [119, 136, 153], # => #778899
-      :lightslategrey       => [119, 136, 153], # => #778899
-      :lightsteelblue       => [176, 196, 222], # => #b0c4de
-      :lightyellow          => [255, 255, 224], # => #ffffe0
-      :limegreen            => [50,  205,  50], # => #32cd32
-      :linen                => [250, 240, 230], # => #faf0e6
-      :magenta              => [255, 0,   255], # => #ff00ff
-      :mediumaquamarine     => [102, 205, 170], # => #66cdaa
-      :mediumblue           => [0,   0,   205], # => #0000cd
-      :mediumorchid         => [186, 85,  211], # => #ba55d3
-      :mediumpurple         => [147, 112, 219], # => #9370db
-      :mediumseagreen       => [60,  179, 113], # => #3cb371
-      :mediumslateblue      => [123, 104, 238], # => #7b68ee
-      :mediumspringgreen    => [0,   250, 154], # => #00fa9a
-      :mediumturquoise      => [72,  209, 204], # => #48d1cc
-      :mediumvioletred      => [199, 21,  133], # => #c71585
-      :midnightblue         => [25,  25,  112], # => #191970
-      :mintcream            => [245, 255, 250], # => #f5fffa
-      :mistyrose            => [255, 228, 225], # => #ffe4e1
-      :moccasin             => [255, 228, 181], # => #ffe4b5
-      :navajowhite          => [255, 222, 173], # => #ffdead
-      :oldlace              => [253, 245, 230], # => #fdf5e6
-      :olivedrab            => [107, 142, 35 ], # => #6b8e23
-      :orangered            => [255, 69,  0  ], # => #ff4500
-      :orchid               => [218, 112, 214], # => #da70d6
-      :palegoldenrod        => [238, 232, 170], # => #eee8aa
-      :palegreen            => [152, 251, 152], # => #98fb98
-      :paleturquoise        => [175, 238, 238], # => #afeeee
-      :palevioletred        => [219, 112, 147], # => #db7093
-      :papayawhip           => [255, 239, 213], # => #ffefd5
-      :peachpuff            => [255, 218, 185], # => #ffdab9
-      :peru                 => [205, 133, 63 ], # => #cd853f
-      :pink                 => [255, 192, 203], # => #ffc0cb
-      :plum                 => [221, 160, 221], # => #dda0dd
-      :powderblue           => [176, 224, 230], # => #b0e0e6
-      :rosybrown            => [188, 143, 143], # => #bc8f8f
-      :royalblue            => [65,  105, 225], # => #4169e1
-      :saddlebrown          => [139, 69,  19 ], # => #8b4513
-      :salmon               => [250, 128, 114], # => #fa8072
-      :sandybrown           => [244, 164, 96 ], # => #f4a460
-      :seagreen             => [46,  139, 87 ], # => #2e8b57
-      :seashell             => [255, 245, 238], # => #fff5ee
-      :sienna               => [160, 82,  45 ], # => #a0522d
-      :skyblue              => [135, 206, 235], # => #87ceeb
-      :slateblue            => [106, 90,  205], # => #6a5acd
-      :slategray            => [112, 128, 144], # => #708090
-      :slategrey            => [112, 128, 144], # => #708090
-      :snow                 => [255, 250, 250], # => #fffafa
-      :springgreen          => [0,   255, 127], # => #00ff7f
-      :steelblue            => [70,  130, 180], # => #4682b4
-      :tan                  => [210, 180, 140], # => #d2b48c
-      :thistle              => [216, 191, 216], # => #d8bfd8
-      :tomato               => [255, 99,  71 ], # => #ff6347
-      :turquoise            => [64,  224, 208], # => #40e0d0
-      :violet               => [238, 130, 238], # => #ee82ee
-      :wheat                => [245, 222, 179], # => #f5deb3
-      :whitesmoke           => [245, 245, 245], # => #f5f5f5
-      :yellowgreen          => [154, 205, 50 ], # => #9acd32
+      :aliceblue            => [240, 248, 255, nil], # => #f0f8ff
+      :antiquewhite         => [250, 235, 215, nil], # => #faebd7
+      :aquamarine           => [127, 255, 212, nil], # => #7fffd4
+      :azure                => [240, 255, 255, nil], # => #f0ffff
+      :beige                => [245, 245, 220, nil], # => #f5f5dc
+      :bisque               => [255, 228, 196, nil], # => #ffe4c4
+      :blanchedalmond       => [255, 235, 205, nil], # => #ffebcd
+      :blueviolet           => [138, 43,  226, nil], # => #8a2be2
+      :brown                => [165, 42,  42,  nil], # => #a52a2a
+      :burlywood            => [222, 184, 135, nil], # => #deb887
+      :cadetblue            => [95,  158, 160, nil], # => #5f9ea0
+      :chartreuse           => [127, 255, 0,   nil], # => #7fff00
+      :chocolate            => [210, 105, 30,  nil], # => #d2691e
+      :coral                => [255, 127, 80,  nil], # => #ff7f50
+      :cornflowerblue       => [100, 149, 237, nil], # => #6495ed
+      :cornsilk             => [255, 248, 220, nil], # => #fff8dc
+      :crimson              => [220, 20,  60,  nil], # => #dc143c
+      :cyan                 => [0,   255, 255, nil], # => #00ffff
+      :darkblue             => [0,   0,   139, nil], # => #00008b
+      :darkcyan             => [0,   139, 139, nil], # => #008b8b
+      :darkgoldenrod        => [184, 134, 11,  nil], # => #b8860b
+      :darkgray             => [169, 169, 169, nil], # => #a9a9a9
+      :darkgreen            => [0,   100, 0,   nil], # => #006400
+      :darkgrey             => [169, 169, 169, nil], # => #a9a9a9
+      :darkkhaki            => [189, 183, 107, nil], # => #bdb76b
+      :darkmagenta          => [139, 0,   139, nil], # => #8b008b
+      :darkolivegreen       => [85,  107, 47,  nil], # => #556b2f
+      :darkorange           => [255, 140, 0,   nil], # => #ff8c00
+      :darkorchid           => [153, 50,  204, nil], # => #9932cc
+      :darkred              => [139, 0,   0,   nil], # => #8b0000
+      :darksalmon           => [233, 150, 122, nil], # => #e9967a
+      :darkseagreen         => [143, 188, 143, nil], # => #8fbc8f
+      :darkslateblue        => [72,  61,  139, nil], # => #483d8b
+      :darkslategray        => [47,  79,  79,  nil], # => #2f4f4f
+      :darkslategrey        => [47,  79,  79,  nil], # => #2f4f4f
+      :darkturquoise        => [0,   206, 209, nil], # => #00ced1
+      :darkviolet           => [148, 0,   211, nil], # => #9400d3
+      :deeppink             => [255, 20,  147, nil], # => #ff1493
+      :deepskyblue          => [0,   191, 255, nil], # => #00bfff
+      :dimgray              => [105, 105, 105, nil], # => #696969
+      :dimgrey              => [105, 105, 105, nil], # => #696969
+      :dodgerblue           => [30,  144, 255, nil], # => #1e90ff
+      :firebrick            => [178, 34,  34,  nil], # => #b22222
+      :floralwhite          => [255, 250, 240, nil], # => #fffaf0
+      :forestgreen          => [34,  139, 34,  nil], # => #228b22
+      :gainsboro            => [220, 220, 220, nil], # => #dcdcdc
+      :ghostwhite           => [248, 248, 255, nil], # => #f8f8ff
+      :gold                 => [255, 215, 0,   nil], # => #ffd700
+      :goldenrod            => [218, 165, 32,  nil], # => #daa520
+      :greenyellow          => [173, 255, 47,  nil], # => #adff2f
+      :grey                 => [128, 128, 128, nil], # => #808080
+      :honeydew             => [240, 255, 240, nil], # => #f0fff0
+      :hotpink              => [255, 105, 180, nil], # => #ff69b4
+      :indianred            => [205, 92,  92,  nil], # => #cd5c5c
+      :indigo               => [75,  0,   130, nil], # => #4b0082
+      :ivory                => [255, 255, 240, nil], # => #fffff0
+      :khaki                => [240, 230, 140, nil], # => #f0e68c
+      :lavender             => [230, 230, 250, nil], # => #e6e6fa
+      :lavenderblush        => [255, 240, 245, nil], # => #fff0f5
+      :lawngreen            => [124, 252, 0,   nil], # => #7cfc00
+      :lemonchiffon         => [255, 250, 205, nil], # => #fffacd
+      :lightblue            => [173, 216, 230, nil], # => #add8e6
+      :lightcoral           => [240, 128, 128, nil], # => #f08080
+      :lightcyan            => [224, 255, 255, nil], # => #e0ffff
+      :lightgoldenrodyellow => [250, 250, 210, nil], # => #fafad2
+      :lightgray            => [211, 211, 211, nil], # => #d3d3d3
+      :lightgreen           => [144, 238, 144, nil], # => #90ee90
+      :lightgrey            => [211, 211, 211, nil], # => #d3d3d3
+      :lightpink            => [255, 182, 193, nil], # => #ffb6c1
+      :lightsalmon          => [255, 160, 122, nil], # => #ffa07a
+      :lightseagreen        => [32,  178, 170, nil], # => #20b2aa
+      :lightskyblue         => [135, 206, 250, nil], # => #87cefa
+      :lightslategray       => [119, 136, 153, nil], # => #778899
+      :lightslategrey       => [119, 136, 153, nil], # => #778899
+      :lightsteelblue       => [176, 196, 222, nil], # => #b0c4de
+      :lightyellow          => [255, 255, 224, nil], # => #ffffe0
+      :limegreen            => [50,  205, 50,  nil], # => #32cd32
+      :linen                => [250, 240, 230, nil], # => #faf0e6
+      :magenta              => [255, 0,   255, nil], # => #ff00ff
+      :mediumaquamarine     => [102, 205, 170, nil], # => #66cdaa
+      :mediumblue           => [0,   0,   205, nil], # => #0000cd
+      :mediumorchid         => [186, 85,  211, nil], # => #ba55d3
+      :mediumpurple         => [147, 112, 219, nil], # => #9370db
+      :mediumseagreen       => [60,  179, 113, nil], # => #3cb371
+      :mediumslateblue      => [123, 104, 238, nil], # => #7b68ee
+      :mediumspringgreen    => [0,   250, 154, nil], # => #00fa9a
+      :mediumturquoise      => [72,  209, 204, nil], # => #48d1cc
+      :mediumvioletred      => [199, 21,  133, nil], # => #c71585
+      :midnightblue         => [25,  25,  112, nil], # => #191970
+      :mintcream            => [245, 255, 250, nil], # => #f5fffa
+      :mistyrose            => [255, 228, 225, nil], # => #ffe4e1
+      :moccasin             => [255, 228, 181, nil], # => #ffe4b5
+      :navajowhite          => [255, 222, 173, nil], # => #ffdead
+      :oldlace              => [253, 245, 230, nil], # => #fdf5e6
+      :olivedrab            => [107, 142, 35,  nil], # => #6b8e23
+      :orangered            => [255, 69,  0,   nil], # => #ff4500
+      :orchid               => [218, 112, 214, nil], # => #da70d6
+      :palegoldenrod        => [238, 232, 170, nil], # => #eee8aa
+      :palegreen            => [152, 251, 152, nil], # => #98fb98
+      :paleturquoise        => [175, 238, 238, nil], # => #afeeee
+      :palevioletred        => [219, 112, 147, nil], # => #db7093
+      :papayawhip           => [255, 239, 213, nil], # => #ffefd5
+      :peachpuff            => [255, 218, 185, nil], # => #ffdab9
+      :peru                 => [205, 133, 63,  nil], # => #cd853f
+      :pink                 => [255, 192, 203, nil], # => #ffc0cb
+      :plum                 => [221, 160, 221, nil], # => #dda0dd
+      :powderblue           => [176, 224, 230, nil], # => #b0e0e6
+      :rosybrown            => [188, 143, 143, nil], # => #bc8f8f
+      :royalblue            => [65,  105, 225, nil], # => #4169e1
+      :saddlebrown          => [139, 69,  19,  nil], # => #8b4513
+      :salmon               => [250, 128, 114, nil], # => #fa8072
+      :sandybrown           => [244, 164, 96,  nil], # => #f4a460
+      :seagreen             => [46,  139, 87,  nil], # => #2e8b57
+      :seashell             => [255, 245, 238, nil], # => #fff5ee
+      :sienna               => [160, 82,  45,  nil], # => #a0522d
+      :skyblue              => [135, 206, 235, nil], # => #87ceeb
+      :slateblue            => [106, 90,  205, nil], # => #6a5acd
+      :slategray            => [112, 128, 144, nil], # => #708090
+      :slategrey            => [112, 128, 144, nil], # => #708090
+      :snow                 => [255, 250, 250, nil], # => #fffafa
+      :springgreen          => [0,   255, 127, nil], # => #00ff7f
+      :steelblue            => [70,  130, 180, nil], # => #4682b4
+      :tan                  => [210, 180, 140, nil], # => #d2b48c
+      :thistle              => [216, 191, 216, nil], # => #d8bfd8
+      :tomato               => [255, 99,  71,  nil], # => #ff6347
+      :turquoise            => [64,  224, 208, nil], # => #40e0d0
+      :violet               => [238, 130, 238, nil], # => #ee82ee
+      :wheat                => [245, 222, 179, nil], # => #f5deb3
+      :whitesmoke           => [245, 245, 245, nil], # => #f5f5f5
+      :yellowgreen          => [154, 205, 50,  nil], # => #9acd32
       
       # The 'transparent' keyword, added to the CSS color spec in CSS3.
       #
